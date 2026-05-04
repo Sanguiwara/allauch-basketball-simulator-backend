57d21b7fb2e55c6d5f051a8c1efad5ae292a1073
Sanguiwara <sanguiwara@hotmail.fr>
2026-05-04 17:04:11 -0500

    test: replace @MockBean with @MockitoBean annotations in controller tests

--- api/src/test/java/com/sanguiwara/controller/GameControllerScheduleTest.java
+++ api/src/test/java/com/sanguiwara/controller/GameControllerScheduleTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,16 +33,16 @@ class GameControllerScheduleTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private GameService gameService;
 
-    @MockBean
+    @MockitoBean
     private GameDTOMapper gameDTOMapper;
 
-    @MockBean
+    @MockitoBean
     private GameSchedulingService gameSchedulingService;
 
-    @MockBean
+    @MockitoBean
     private EventManager eventManager;
 
     @Test
